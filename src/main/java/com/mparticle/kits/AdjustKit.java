07218e7655f6ee593ec75901bd5ccc8e468dd985
Sam Dozor <sdozor@mparticle.com>
2016-05-18 12:31:35 -0400

    Simplify Adjust Kit, rely on Kit Activity forwarding

--- src/main/java/com/mparticle/kits/AdjustKit.java
+++ src/main/java/com/mparticle/kits/AdjustKit.java
@@ -24,30 +24,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class AdjustKit extends KitIntegration implements KitIntegration.ActivityListener{
 
     private static final String APP_TOKEN = "appToken";
-    boolean initialized = false;
-    private AtomicBoolean hasResumed = new AtomicBoolean(false);
-
-    private void initAdjust(){
-        if (!initialized) {
-            boolean production = MParticle.Environment.Production.equals(MParticle.getInstance().getEnvironment());
-
-            AdjustConfig config = new AdjustConfig(getContext(),
-                    getSettings().get(APP_TOKEN),
-                    production ? AdjustConfig.ENVIRONMENT_PRODUCTION : AdjustConfig.ENVIRONMENT_SANDBOX);
-            if (!production){
-                config.setLogLevel(LogLevel.VERBOSE);
-            }
-            config.setEventBufferingEnabled(false);
-            Adjust.onCreate(config);
-            if (!isBackgrounded()) {
-                if (!hasResumed.get()) {
-                    Adjust.onResume();
-                    hasResumed.set(true);
-                }
-            }
-            initialized = true;
-        }
-    }
 
     @Override
     public Object getInstance() {
@@ -61,7 +37,17 @@ public class AdjustKit extends KitIntegration implements KitIntegration.Activity
 
     @Override
     protected List<ReportingMessage> onKitCreate(Map<String, String> settings, Context context) {
-        initAdjust();
+        boolean production = MParticle.Environment.Production.equals(MParticle.getInstance().getEnvironment());
+
+        AdjustConfig config = new AdjustConfig(getContext(),
+                getSettings().get(APP_TOKEN),
+                production ? AdjustConfig.ENVIRONMENT_PRODUCTION : AdjustConfig.ENVIRONMENT_SANDBOX);
+
+        if (!production){
+            config.setLogLevel(LogLevel.VERBOSE);
+        }
+        config.setEventBufferingEnabled(false);
+        Adjust.onCreate(config);
         return null;
     }
 
@@ -71,23 +57,28 @@ public class AdjustKit extends KitIntegration implements KitIntegration.Activity
     }
 
     @Override
-    public List<ReportingMessage> onActivityResumed(Activity activity) {
-        if (!hasResumed.get()) {
-            Adjust.onResume();
-            hasResumed.set(true);
-            List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
-            messageList.add(
-                new ReportingMessage(this, ReportingMessage.MessageType.APP_STATE_TRANSITION, System.currentTimeMillis(), null)
-            );
-            return messageList;
-        }
+    public List<ReportingMessage> onActivityCreated(Activity activity, Bundle bundle) {
         return null;
+    }
+
+    @Override
+    public List<ReportingMessage> onActivityStarted(Activity activity) {
+        return null;
+    }
+
+    @Override
+    public List<ReportingMessage> onActivityResumed(Activity activity) {
+        Adjust.onResume();
+        List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
+        messageList.add(
+            new ReportingMessage(this, ReportingMessage.MessageType.APP_STATE_TRANSITION, System.currentTimeMillis(), null)
+        );
+        return messageList;
     }
 
     @Override
     public List<ReportingMessage> onActivityPaused(Activity activity) {
         Adjust.onPause();
-        hasResumed.set(false);
         List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
         messageList.add(
                 new ReportingMessage(this, ReportingMessage.MessageType.APP_STATE_TRANSITION, System.currentTimeMillis(), null)
@@ -101,27 +92,13 @@ public class AdjustKit extends KitIntegration implements KitIntegration.Activity
     }
 
     @Override
-    public List<ReportingMessage> onActivitySaveInstanceState(Activity activity, Bundle outState) {
+    public List<ReportingMessage> onActivitySaveInstanceState(Activity activity, Bundle bundle) {
         return null;
     }
 
     @Override
     public List<ReportingMessage> onActivityDestroyed(Activity activity) {
         return null;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        return null;
-    }
-
-    @Override
-    public List<ReportingMessage> onActivityStarted(Activity activity) {
-        List<ReportingMessage> messageList = new LinkedList<ReportingMessage>();
-        messageList.add(
-                new ReportingMessage(this, ReportingMessage.MessageType.APP_STATE_TRANSITION, System.currentTimeMillis(), null)
-        );
-        return messageList;
     }
 
     @Override
@@ -134,7 +111,4 @@ public class AdjustKit extends KitIntegration implements KitIntegration.Activity
         );
         return messageList;
     }
-
-
-
 }
